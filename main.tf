25ef9e8c7c67669492e77f6221db929e9644041c
abobocea211 <adelinbobocea91@gmail.com>
2023-11-24 10:19:35 +0100

    changing ami

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-07355fe79b493752d" # us-west-2
+  ami           = "ami-07355fe79b493752d" 
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
